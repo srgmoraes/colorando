5c1aa7e2ef2fd4b99cf355d5e4fa584dfa878dbf
hugo.andrade <andradehugo@id.uff.br>
2021-11-23 18:25:51 -0300

    commit inicial configurações

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,22 +1,16 @@
-import 'package:colorando3/config_page.dart';
 import 'package:colorando3/intro_page.dart';
-import 'package:colorando3/mydraws_page.dart';
-import 'package:colorando3/navigate_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-
-          home:IntroPage(),
-        // routes: {
-        //   '/': (context) => NavigatePage(),
-        //   '/mydraws': (context) => MyDrawsPage(),
-        //   '/config': (context) => ConfigPage(),
-        // }
-      );
+    return MaterialApp(
+      home: IntroPage(),
+      // routes: {
+      //   '/': (context) => NavigatePage(),
+      //   '/mydraws': (context) => MyDrawsPage(),
+      //   '/config': (context) => ConfigPage(),
+      // }
+    );
   }
 }
-
-
--- lib/library_page.dart
+++ lib/library_page.dart
@@ -1,22 +1,27 @@
+import 'package:colorando3/settings.dart';
 import 'package:flutter/material.dart';
 
-class LibraryPage extends StatelessWidget  {
-
+class LibraryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    Settings.playBackgroundSong();
+
+    return Scaffold(
       body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(20, (index) {
-        return Center(
-          child: Card(
-          color: Colors.white,
-          child: Container(child: Image.network('https://anagiovanna.com.br/blog/wp-content/uploads/2020/12/desenhos-de-unicornio-para-pintar-e-colorir-22-750x925.jpg'), width: 200, height: 200,),
-          ),
-        );
-      })
-      ),
+          crossAxisCount: 2,
+          children: List.generate(20, (index) {
+            return Center(
+              child: Card(
+                color: Colors.white,
+                child: Container(
+                  child: Image.network(
+                      'https://anagiovanna.com.br/blog/wp-content/uploads/2020/12/desenhos-de-unicornio-para-pintar-e-colorir-22-750x925.jpg'),
+                  width: 200,
+                  height: 200,
+                ),
+              ),
+            );
+          })),
     );
   }
-
 }
