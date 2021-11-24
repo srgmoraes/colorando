b2381e2c2479be4c740f2d8b03a7f6c23edd8603
hugo.andrade <andradehugo@id.uff.br>
2021-11-23 22:29:11 -0300

    inicio settings

--- lib/config_page.dart
+++ lib/config_page.dart
@@ -1,8 +1,55 @@
+import 'package:colorando3/settings.dart';
 import 'package:flutter/material.dart';
 
 class ConfigPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SwitchListTileDemo(),
+    );
+  }
+}
+
+class SwitchListTileDemo extends StatefulWidget {
+  @override
+  _SwitchListTileDemoState createState() => _SwitchListTileDemoState();
+}
+
+class _SwitchListTileDemoState extends State<SwitchListTileDemo> {
+  bool _flutter = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Align(
+        alignment: Alignment.topLeft,
+        child: Card(
+          color: Colors.white,
+          child: SwitchListTile(
+            title: Text(
+              'Música',
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 20),
+            ),
+            value: _flutter,
+            activeColor: Colors.purple,
+            inactiveTrackColor: Colors.grey,
+            onChanged: (bool value) {
+              Settings.changeBackgroundSong(value);
+              setState(() {
+                _flutter = value;
+              });
+            },
+            secondary: Image.asset(
+              "assets/music.png",
+            ),
+            controlAffinity: ListTileControlAffinity.trailing,
+          ),
+        ),
+      ),
+    );
   }
 }
